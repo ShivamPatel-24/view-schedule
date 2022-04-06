1e7aa051fe193016cd31f1fc54f0912c70178e27
div <div.pat499@gmail.com>
2022-04-06 17:42:55 -0400

    Added Button to select day

--- script.js
+++ script.js
@@ -1,66 +1,90 @@
+function loadTable(dow) {
+    var fileName = "/schedule/" + dow + ".csv"
 
-var fileName = "/schedule/monday.csv"
+    d3.csv(fileName).then(function (data) {
 
-d3.csv(fileName).then( function(data) {
+        console.log(fileName);
 
-    console.log(data);
+        var col_names = ["Id"].concat(Object.keys(data[0]))
 
-    var table = d3.select('body').append('table');
-    var thead = table.append('thead');
-    var tbody = table.append('tbody');
+        for (let k in data) {
+            data[k]["Id"] = parseInt(k) + 1
+            // console.log("data", data[k])
+        }
 
-
-    var col_names = Object.keys(data[0])
-    console.log(col_names)
-    thead.append('tr')
-	  .selectAll('th')
-	  .data(col_names).enter()
-	  .append('th')
-	    .text( (c) => {return c});
-
-    var rows = tbody.selectAll('tr')
-	  .data(data)
-	  .enter()
-	  .append('tr');
-
-      var cells = rows.selectAll('td')
-	  .data(function (row) {
-	    return col_names.map(function (column) {
-	      return {column: column, value: row[column]};
-	    });
-	  })
-	  .enter()
-	  .append('td')
-	    .text(function (d) { return d.value; });
-
-  return table;
-
-});
+        var table = d3.select('#tble').append('table');
+        var thead = table.append('thead');
+        var tbody = table.append('tbody');
 
 
+        // console.log(col_names)
+        thead.append('tr')
+            .selectAll('th')
+            .data(col_names).enter()
+            .append('th')
+            .text((c) => { return c });
 
-function date_time(){
+        var rows = tbody.selectAll('tr')
+            .data(data)
+            .enter()
+            .append('tr');
+
+        var cells = rows.selectAll('td')
+            .data(function (row) {
+                //   console.log(col_names.map(function (column) {
+                //     return {column: column, value: row[column]};
+                //   }))
+                return col_names.map(function (column) {
+                    return { column: column, value: row[column] };
+                });
+            })
+            .enter()
+            .append('td')
+            .text(function (d) { return d.value; })
+
+        return table;
+
+    });
+
+}
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function date_time() {
     var d = new Date();
 
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     var hours = d.getHours()
     var minutes = d.getMinutes()
     var sec = d.getSeconds()
 
-    if (minutes < 10){ minutes = "0" + minutes }
-    if (sec < 10){ sec = "0" + sec }
+    if (minutes < 10) { minutes = "0" + minutes }
+    if (sec < 10) { sec = "0" + sec }
 
     var time = hours + ":" + minutes + ":" + sec + " ";
-    if(hours > 11){ time += "PM"; } else { time += "AM"; }
+    if (hours > 11) { time += "PM"; } else { time += "AM"; }
 
     var dateTime = days[d.getDay()] + " - " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear() + ", " + time;
-    
-    console.log("Date time:",date_time)
-    document.getElementById('date').innerHTML= dateTime;
 
-    setTimeout(date_time,1000);
+    document.getElementById('date').innerHTML = dateTime;
+
+    setTimeout(date_time, 1000);
 }
 
 date_time();
+
+let today = (days[(new Date()).getDay()].toLowerCase());
+var df = document.getElementById(today);
+df.selected = true
+
+
+loadTable(today)
+
+
+function getOption(event) {
+    selectElement = document.querySelector('#dayofweek');
+    console.log(event.target.value)
+    document.getElementById("tble").innerHTML = ""
+    loadTable(event.target.value)
+}
+
